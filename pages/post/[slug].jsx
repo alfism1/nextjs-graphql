e7e9af4a062716f6c641a493db5b9db0f8a96427
alfism <alfialfarisi@gmail.com>
2021-11-14 15:14:30 +0700

    Add social media meta

--- pages/post/[slug].jsx
+++ pages/post/[slug].jsx
@@ -1,16 +1,29 @@
-import React, { } from 'react';
-import { useRouter } from 'next/router'
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 import Head from "next/head";
 
-import { PostWidget, Categories, PostDetail, Author, Comments, CommentsForm, Loader } from '../../components'
+import {
+  PostWidget,
+  Categories,
+  PostDetail,
+  Author,
+  Comments,
+  CommentsForm,
+  Loader,
+} from "../../components";
 
-import { getPostDetails, getPosts } from "../../services"
+import { getPostDetails, getPosts } from "../../services";
 
 function PostDetails({ post }) {
   const router = useRouter();
+  const [origin, setOrigin] = useState();
+
+  useEffect(() => {
+    setOrigin(window.location.origin);
+  }, [])
 
   if (router.isFallback) {
-    return <Loader />
+    return <Loader />;
   }
 
   return (
@@ -20,6 +33,18 @@ function PostDetails({ post }) {
         <link rel="icon" href="/favicon.ico" />
         <meta name="description" content={post.excerpt} />
         <link rel="image_src" href={post.coverImage.url} />
+
+        {/* social media meta */}
+        <meta property="og:title" content={post.title} />
+        <meta property="og:type" content="article" />
+        <meta property="og:url" content={`${origin}/post/${post.slug}`} />
+        <meta property="og:image" content={`${post.coverImage.url}`} />
+        <meta property="og:description" content={post.excerpt} />
+
+        <meta name="twitter:title" content={post.title} />
+        <meta name="twitter:description" content={post.excerpt} />
+        <meta name="twitter:image" content={post.coverImage.url} />
+        <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-0 lg:gap-12">
         <div className="col-span-1 lg:col-span-8">
@@ -40,14 +65,13 @@ function PostDetails({ post }) {
 }
 export default PostDetails;
 
-
 export async function getStaticProps({ params }) {
   const data = await getPostDetails(params.slug);
 
   return {
     props: { post: data },
     revalidate: 10,
-  }
+  };
 }
 
 export async function getStaticPaths() {
@@ -56,7 +80,7 @@ export async function getStaticPaths() {
   return {
     paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
     fallback: true,
-  }
+  };
 }
 
 // export async function getServerSideProps({ params }) {

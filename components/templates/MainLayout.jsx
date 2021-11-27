0c6948c601b8cbd8ddc1e09004339dd8738a0beb
alfism <alfialfarisi@gmail.com>
2021-11-27 21:21:23 +0700

    Homepage add under maintenance

--- components/templates/MainLayout.jsx
+++ components/templates/MainLayout.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import Link from "next/link"
+import React from "react";
+import Link from "next/link";
 
 function MainLayout(props) {
   return (
-    <div>
-      Welcome to our channles!
-      <Link href="/blog">Click Blog</Link>
+    <div className="flex h-screen flex-col justify-center items-center">
+      <h1>Under maintenance!</h1>
+      <Link href="/blog">
+        <div className="block bg-green-200 rounded-full border-2 border-green-500 px-3 py-1 mt-3 transition-all duration-150 hover:scale-110" role="button">Click here</div>
+      </Link>
     </div>
   );
 }

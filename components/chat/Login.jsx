8848107f50709584c76f49cfab47a58284e3f56d
alfism1 <alfialfarisi@gmail.com>
2021-12-29 05:58:08 +0700

    Update Login page

--- components/chat/Login.jsx
+++ components/chat/Login.jsx
@@ -9,6 +9,7 @@ const UserClient = ({ setChatUsername, setChatColor }) => {
   const [username, setUsername] = useState("");
   const [message, setMessage] = useState("");
   const [color, setColor] = useState("gray");
+  const [bgColor, setBgColor] = useState("bg-red-800");
 
   const [_getChatUsername, saveChatUsername] = useLocalStorage(
     "chatUsername",
@@ -67,6 +68,11 @@ const UserClient = ({ setChatUsername, setChatColor }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
+    if (usernameRef.current.value.length <= 0) {
+      usernameRef.current.focus();
+      setMessage("Please enter any username");
+      return;
+    }
     saveChatUsername(usernameRef.current.value); // set username localstorage
     setChatUsername(usernameRef.current.value); // set username redux
     saveChatColor(color); // set color localstorage
@@ -87,8 +93,10 @@ const UserClient = ({ setChatUsername, setChatColor }) => {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center mt-10 p-3">
-      <div className="w-full max-w-lg border p-4">
+    <div
+      className={`flex flex-col h-screen items-center p-3 transition-all duration-300 ${bgColor}`}
+    >
+      <div className="w-full max-w-lg border p-4 bg-white shadow-2xl mt-10">
         <h2 className="text-xl border-b border-gray-200 mb-4">
           Enter any username
         </h2>
@@ -109,6 +117,7 @@ const UserClient = ({ setChatUsername, setChatColor }) => {
                   key={c.color}
                   onClick={() => {
                     setColor(c.color);
+                    setBgColor(c.bg);
                   }}
                   className={`inline-block ${c.bg} ${c.bgHover} w-3 h-3 rounded-full cursor-pointer`}
                 />
